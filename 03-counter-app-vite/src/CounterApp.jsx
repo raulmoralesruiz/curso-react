d08a4c11c211b7a7308cfcd08c556d843c920ce3
Raúl Morales <raulmdaw@gmail.com>
2022-09-17 12:24:45 +0200

    first hook -> useState

--- 03-counter-app-vite/src/CounterApp.jsx
+++ 03-counter-app-vite/src/CounterApp.jsx
@@ -1,14 +1,28 @@
+import { useState } from "react";
 import PropTypes from "prop-types";
 
-export const CounterApp = ({value}) => {
+// const click = (event) => {
+//   console.log(event);
+// };
+
+export const CounterApp = ({ value }) => {
+
+  const [counter, setCounter] = useState(value);
+  
+  const click = () => {
+    setCounter(counter + 1);
+  };
+
   return (
     <>
       <h1>CounterApp</h1>
-      <h2>{value}</h2>
-    </>
-  )
-}
+      <h2>{counter}</h2>
 
-CounterApp.propTypes  = {
+      <button onClick={click}> +1 </button>
+    </>
+  );
+};
+
+CounterApp.propTypes = {
   value: PropTypes.number.isRequired,
 };
