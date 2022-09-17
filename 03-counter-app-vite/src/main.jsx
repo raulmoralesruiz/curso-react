f42c7c4ab3a15aad6389a10f6937c69ee2bf87da
Raúl Morales <raulmdaw@gmail.com>
2022-09-17 11:58:50 +0200

    03-counter-app v1

--- 03-counter-app-vite/src/main.jsx
+++ 03-counter-app-vite/src/main.jsx
@@ -1,11 +1,11 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
-import { FirstApp } from "./FirstApp";
+import { CounterApp } from "./CounterApp";
 
 import "./styles.css";
 
 ReactDOM.createRoot(document.getElementById("root")).render(
   <React.StrictMode>
-    <FirstApp/>
+    <CounterApp value={22}/>
   </React.StrictMode>
 );
