b8df4052cf5cacca2c759c9954bd2e1e9beeac45
Raúl Morales <raulmdaw@gmail.com>
2022-10-01 16:29:10 +0200

    05-hook-app | useEffect precauciones

--- 05-hook-app/src/02-useEffect/Message.jsx
+++ 05-hook-app/src/02-useEffect/Message.jsx
@@ -1,18 +1,36 @@
-import { useEffect } from "react";
+import { useEffect, useState } from "react";
 
 export const Message = () => {
+  const [coords, setCoords] = useState({ x: 0, y: 0 });
 
   useEffect(() => {
-    console.log("Message mounted")
+    // se monta el componente
+    const onMouseMove = ({ x, y }) => {
+      // const coords = { x, y };
+      setCoords({ x, y });
+    };
+
+    window.addEventListener("mousemove", onMouseMove);
 
     return () => {
-      console.log("Message unmounted")
+      // se desmonta el componente
+      window.removeEventListener("mousemove", onMouseMove);
     };
   }, []);
 
   return (
     <>
       <h3>El usuario ya existe</h3>
+      {/* { JSON.stringify(coords)} */}
+
+      <div className="d-flex">
+        <div>
+          <span className="badge bg-info">x: {coords.x}</span>
+        </div>
+        <div>
+          <span className="badge bg-primary">y: {coords.y}</span>
+        </div>
+      </div>
     </>
-  )
-}
+  );
+};
