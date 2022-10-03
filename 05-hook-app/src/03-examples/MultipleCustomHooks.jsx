2470490cf0b8d41838ec4609ff00a3b68d4f167a
Raúl Morales <raulmdaw@gmail.com>
2022-10-03 20:58:05 +0200

    05-hook-app | Conectando ambos hooks

--- 05-hook-app/src/03-examples/MultipleCustomHooks.jsx
+++ 05-hook-app/src/03-examples/MultipleCustomHooks.jsx
@@ -3,13 +3,27 @@ import { useFetch } from "../hooks/useFetch"
 export const MultipleCustomHooks = () => {
   
   const { data, loading, error } = useFetch(`https://www.breakingbadapi.com/api/quotes/1`);
-  console.log({ data, loading, error });
+  
+  const { author, quote } = !!data && data[0];
 
   return (
     <>
       <h1>Breaking bad quotes</h1>
-      <hr/>
-      
+      <hr />
+
+      {loading ? (
+        <div className="alert alert-info text-center">Loading...</div>
+      ) : (
+        <blockquote className="blockquote text-end">
+          <p className="mb-1">{quote}</p>
+          <footer className="blockquote-footer">{author}</footer>
+        </blockquote>
+      )}
+
+      <button className="btn btn-primary">
+        Next quote
+      </button>
+
     </>
-  )
+  );
 }
