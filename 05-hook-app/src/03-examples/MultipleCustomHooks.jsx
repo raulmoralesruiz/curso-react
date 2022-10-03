34e99089bab45798f067de5be0789478a7d82aa4
Raúl Morales <raulmdaw@gmail.com>
2022-10-03 21:20:09 +0200

    05-hook-app | Optimizando el código

--- 05-hook-app/src/03-examples/MultipleCustomHooks.jsx
+++ 05-hook-app/src/03-examples/MultipleCustomHooks.jsx
@@ -1,29 +1,36 @@
-import { useFetch } from "../hooks/useFetch"
+import { useCounter, useFetch } from "../hooks";
+import { LoadingQuote, Quote } from "./";
 
 export const MultipleCustomHooks = () => {
   
-  const { data, loading, error } = useFetch(`https://www.breakingbadapi.com/api/quotes/1`);
+  // useCounter initial state is 1
+  const { counter, increment } = useCounter(1);  
+
+  const { loading, data, error } = useFetch(
+    `https://www.breakingbadapi.com/api/quotes/${counter}`
+  );
   
   const { author, quote } = !!data && data[0];
+
+  
 
   return (
     <>
       <h1>Breaking bad quotes</h1>
       <hr />
 
-      {loading ? (
-        <div className="alert alert-info text-center">Loading...</div>
-      ) : (
-        <blockquote className="blockquote text-end">
-          <p className="mb-1">{quote}</p>
-          <footer className="blockquote-footer">{author}</footer>
-        </blockquote>
-      )}
+      { loading 
+        ? <LoadingQuote /> 
+        : <Quote author={author} quote={quote} />
+      }
 
-      <button className="btn btn-primary">
+      <button
+        className="btn btn-primary"
+        disabled={loading}
+        onClick={() => increment()}
+      >
         Next quote
       </button>
-
     </>
   );
 }
