ca62add18bda0fc0325a4676bc028b8b6f51c7c9
Raúl Morales <raulmdaw@gmail.com>
2022-10-06 23:24:25 +0200

    05-hook-app | Memo React

--- 05-hook-app/src/main.jsx
+++ 05-hook-app/src/main.jsx
@@ -1,7 +1,8 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
+import { Memorize } from "./06-memos/Memorize";
 // import { FocusScreen } from "./04-useRef/FocusScreen";
-import { Layout } from "./05-useLayoutEffect/Layout";
+// import { Layout } from "./05-useLayoutEffect/Layout";
 // import { CounterApp } from "./01-useState/CounterApp";
 // import { CounterWithCustomHook } from "./01-useState/CounterWithCustomHook";
 // import { FormWithCustomHook } from "./02-useEffect/FormWithCustomHook";
@@ -12,6 +13,6 @@ import "./index.css";
 
 ReactDOM.createRoot(document.getElementById("root")).render(
   // <React.StrictMode>
-    <Layout />
+    <Memorize />
   // </React.StrictMode>
 );
