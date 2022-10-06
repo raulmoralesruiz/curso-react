be470284efce77b804b4739e5aa939930ed643e2
Raúl Morales <raulmdaw@gmail.com>
2022-10-06 23:37:50 +0200

    05-hook-app | useMemo

--- 05-hook-app/src/main.jsx
+++ 05-hook-app/src/main.jsx
@@ -1,6 +1,7 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
-import { Memorize } from "./06-memos/Memorize";
+import { MemoHook } from "./06-memos/MemoHook";
+// import { Memorize } from "./06-memos/Memorize";
 // import { FocusScreen } from "./04-useRef/FocusScreen";
 // import { Layout } from "./05-useLayoutEffect/Layout";
 // import { CounterApp } from "./01-useState/CounterApp";
@@ -13,6 +14,6 @@ import "./index.css";
 
 ReactDOM.createRoot(document.getElementById("root")).render(
   // <React.StrictMode>
-    <Memorize />
+    <MemoHook />
   // </React.StrictMode>
 );
