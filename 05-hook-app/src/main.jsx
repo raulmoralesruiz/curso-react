a5b2048d9338dc7331b0e7fbcf99cddf8362e830
Raúl Morales <raulmdaw@gmail.com>
2022-10-07 17:59:32 +0200

    05-hook-app | tareaMemorize

--- 05-hook-app/src/main.jsx
+++ 05-hook-app/src/main.jsx
@@ -1,19 +1,21 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
-import { MemoHook } from "./06-memos/MemoHook";
-// import { Memorize } from "./06-memos/Memorize";
-// import { FocusScreen } from "./04-useRef/FocusScreen";
-// import { Layout } from "./05-useLayoutEffect/Layout";
+// import { HooksApp } from "./HooksApp";
 // import { CounterApp } from "./01-useState/CounterApp";
 // import { CounterWithCustomHook } from "./01-useState/CounterWithCustomHook";
+// import { SimpleForm } from "./02-useEffect/SimpleForm";
 // import { FormWithCustomHook } from "./02-useEffect/FormWithCustomHook";
 // import { MultipleCustomHooks } from "./03-examples/MultipleCustomHooks";
-// import { SimpleForm } from "./02-useEffect/SimpleForm";
-// import { HooksApp } from "./HooksApp";
+// import { FocusScreen } from "./04-useRef/FocusScreen";
+// import { Layout } from "./05-useLayoutEffect/Layout";
+// import { Memorize } from "./06-memos/Memorize";
+// import { MemoHook } from "./06-memos/MemoHook";
+// import { CallbackHook } from "./06-memos/CallbackHook";
+import { Padre } from "./07-tarea-memo/Padre";
 import "./index.css";
 
 ReactDOM.createRoot(document.getElementById("root")).render(
   // <React.StrictMode>
-    <MemoHook />
+    <Padre />
   // </React.StrictMode>
 );
