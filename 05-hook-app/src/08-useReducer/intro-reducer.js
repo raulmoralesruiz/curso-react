dc5f7821d326f76c85a68094d092917f07bdd8fd
Raúl Morales <raulmdaw@gmail.com>
2022-10-20 21:28:16 +0200

    05-hook-app | Idea general

--- 05-hook-app/src/08-useReducer/intro-reducer.js
+++ 05-hook-app/src/08-useReducer/intro-reducer.js
@@ -1,1 +1,31 @@
-console.log("test");
+const initialState = [{
+  id: 1,
+  todo: 'Comprar pan',
+  done: false 
+}];
+
+const todoReducer = (state = initialState, action = {}) => {
+
+  if (action.type === 'add') {
+    return [...state, action.payload];
+  }
+
+  return state;
+}
+
+let todos = todoReducer();
+
+const newTodo = {
+  id: 2,
+  todo: 'Comprar leche',
+  done: false
+}
+
+const addTodoAction = {
+  type: 'add',
+  payload: newTodo
+}
+
+todos = todoReducer(todos, addTodoAction);
+
+console.log({state: todos});
